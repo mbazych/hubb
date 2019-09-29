da44d11200cba377d1bea4e55167e073f717c69a
mbazych <44398475+mbazych@users.noreply.github.com>
2019-09-30 00:03:15 +0200

    asd
    asd

--- src/components/Hub.js
+++ src/components/Hub.js
@@ -11,7 +11,7 @@ class Hub extends Component {
                         <HubPanel
                             src="https://static.wirtualnemedia.pl/media/top/goscniedzielny-logo655.png"
                             title="Wpisz na listę gości"
-                            text="Aplikacja do wpisywania na listę gości używana na firmowym tablecie"
+                            text="Aplikacja do wpisywania na listę gości używana na firmowym tablecie."
                             link="/guestlist"
                         />
                     </Col>
@@ -19,7 +19,7 @@ class Hub extends Component {
                         <HubPanel
                             src="https://apollo-ireland.akamaized.net/v1/files/eyJmbiI6ImljNnVuMTB2ODMzZzItT1RPTU9UT1BMIn0.4sfH0TA7Y1nz5L3BBtXyTYRWq4sNuYCIaLlGEw1Bw2U/image;s=1080x720;cars_;/902763457_;slot=1;filename=eyJmbiI6ImljNnVuMTB2ODMzZzItT1RPTU9UT1BMIn0.4sfH0TA7Y1nz5L3BBtXyTYRWq4sNuYCIaLlGEw1Bw2U_rev001.jpg"
                             title="Wypożycz firmowy samochód"
-                            text="Dzięki tej aplikacji w łatwy sposób wypożyczysz samochód firmowy"
+                            text="Dzięki tej aplikacji w łatwy sposób wypożyczysz samochód firmowy."
                             link="/car"
                         />
                     </Col>
